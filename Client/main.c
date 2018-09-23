2d10fc16cb80436149deae92ec5ba1c3356d5e50
X-death25 <X-death@ultimate-consoles.fr>
2018-09-23 12:34:26 +0200

    Add Simple Menu

--- Client/main.c
+++ Client/main.c
@@ -33,41 +33,42 @@
 int main()
 {
 
-  // LibFTDI specific var
+    // LibFTDI specific var
 
     int ret; // LibFTDI var for return info
     struct ftdi_context *ftdi;
     struct ftdi_version_info version;
-	struct ftdi_transfer_control *wr_transfer, *rd_transfer;
+    struct ftdi_transfer_control *wr_transfer, *rd_transfer;
 
-	int baudrate = 115200;
-	unsigned char tx_buf[128];
-	unsigned char rx_buf[128];
-    
-   // Wonder X Specific Var
-	
-	int i=0;
+    int baudrate = 115200;
+    unsigned char tx_buf[128];
+    unsigned char rx_buf[128];
 
-   // Main Program   
+    // Wonder X Specific Var
+
+    int i=0;
+    int choixMenu=0;
+
+    // Main Program
 
     printf("\n");
     printf(" ---------------------------------\n");
     printf("    WonderX Flasher Software      \n");
     printf(" ---------------------------------\n");
-	printf("\n");
+    printf("\n");
 
-	// Init LibFTDI
+    // Init LibFTDI
 
 
     if ((ftdi = ftdi_new()) == 0)
-   {
+    {
         fprintf(stderr, "ftdi_new failed\n");
         return EXIT_FAILURE;
     }
 
-printf("Detecting FTDI Chip...\n");
-	
-	// Try to open FTDI with correct id
+    printf("Detecting FTDI Chip...\n");
+
+    // Try to open FTDI with correct id
 
     if ((ret = ftdi_usb_open(ftdi, 0x0403, 0x6001)) < 0)
     {
@@ -76,7 +77,7 @@ printf("Detecting FTDI Chip...\n");
         return EXIT_FAILURE;
     }
 
-	printf("FTDI chip detected sucessfully !\n");
+    printf("FTDI chip detected sucessfully !\n");
 
     // Read out FTDIChip-ID of R type chips
 
@@ -87,8 +88,8 @@ printf("Detecting FTDI Chip...\n");
         printf("FTDI chipid: %X\n", chipid);
     }
 
- // Set baudrate
-   
+// Set baudrate
+
     if ((ret = ftdi_set_baudrate(ftdi, baudrate)) < 0)
     {
         fprintf(stderr, "unable to set baudrate: %d (%s)\n", ret, ftdi_get_error_string(ftdi));
@@ -98,62 +99,96 @@ printf("Detecting FTDI Chip...\n");
 
 // Set BItbang Mode
 
-   printf("Enabling bitbang mode \n");
-   ftdi_set_bitmode(ftdi, 0xFF, BITMODE_BITBANG);
-  // ftdi_set_bitmode(ftdi, 0xFF, BITMODE_RESET); // switch off bitbang mode, back to regular serial/FIFO 
+    printf("Enabling bitbang mode");
+    ftdi_set_bitmode(ftdi, 0xFF, BITMODE_BITBANG);
+    // ftdi_set_bitmode(ftdi, 0xFF, BITMODE_RESET); // switch off bitbang mode, back to regular serial/FIFO
 
 
 // Clean Buffer
-	printf("\n");
 
 
-	for (i=0; i<128; i++) 
-	{
-		tx_buf[i]=0xFF;
-	    rx_buf[i]=0x00;
-	}
-// Send 128 bytes into the FTDI FIFO
+    for (i=0; i<128; i++)
+    {
+        tx_buf[i]=0xAA;
+        rx_buf[i]=0x00;
+    }
 
-	printf("Writing to the FIFO... \n");	
+// Display Menu
 
-if (ret = ftdi_write_data(ftdi,tx_buf,sizeof(tx_buf)) < 0)
-	{
-	   printf("Write error \n");
-	   exit(-1);
-	}
-	printf("FIFO Writted Sucessfully ! \n");
 
-// Read 128 bytes from the FTDI FIFO
+    printf("\n\n --- MENU ---\n");
+    printf(" 1) Write FT245 FIFO \n");
+    printf(" 2) Read FT245 FIFO \n");
 
-	printf("\n");
-	printf("Reading from the FIFO... \n");
+    printf("\nYour choice: \n");
+    scanf("%d", &choixMenu);
 
-if (ret = ftdi_read_data(ftdi,rx_buf,sizeof(rx_buf)) < 0) 
-	{
-	   printf("Read error \n");
-	   exit(-1);
-	}
+    switch(choixMenu)
+    {
 
-	printf("FIFO Readed Sucessfully ! \n");
+    case 1: // Write FT245 FIFO
+
+        printf("Writing to the FIFO... \n");
+        wr_transfer = ftdi_write_data_submit(ftdi,tx_buf,sizeof(tx_buf));
+
+// Wait for transfers to complete
+
+        if (wr_transfer > 0)
+        {
+            ftdi_transfer_data_done(wr_transfer);
+        }
+        if (wr_transfer < 0)
+        {
+            printf("Write error \n");
+            exit(-1);
+        }
+        printf("FIFO Writted Sucessfully ! \n");
+        ftdi_usb_close(ftdi);
+        ftdi_free(ftdi);
+        break;
+
+    case 2: // Read FT245 FIFO
+
+        printf("\n");
+        printf("Reading from the FIFO... \n");
+        rd_transfer = ftdi_read_data_submit(ftdi,rx_buf,sizeof(rx_buf));
+
+// Wait for transfers to complete
+
+        if (rd_transfer > 0)
+        {
+            ftdi_transfer_data_done(rd_transfer);
+        }
+
+        if (rd_transfer < 0)
+        {
+            printf("read error \n");
+            exit(-1);
+        }
+
+        printf("FIFO Readed Sucessfully ! \n");
 
 // Display FTDI FIFO
 
-	printf("\n");
-	printf("Display FIFO value : \n");
+        printf("\n");
+        printf("Display FIFO value : \n");
 
-			   for (i=0; i<128; i++)
-                {
-                    printf("%02X ", rx_buf[i] & 255);
-                    if (i % 16 == 15 && i < 128) printf("\n");
-                }
+        for (i=0; i<128; i++)
+        {
+            printf("%02X ", rx_buf[i] & 255);
+            if (i % 16 == 15 && i < 128)
+                printf("\n");
+        }
+        ftdi_usb_close(ftdi);
+        ftdi_free(ftdi);
+        break;
+    }
 
-   	ftdi_usb_close(ftdi);
-    ftdi_free(ftdi);
 
 
-   		return 0;
+    return 0;
 
-  
+
 }
 
 
